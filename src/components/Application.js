a41f9e72413d46e1f096113509893bf1e7246cdc
Trigun Joshi <trigunjoshi@gmail.com>
2022-01-24 16:51:46 -0800

    implementing update

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,11 +3,28 @@ import DayList from "./DayList";
 import "components/Application.scss";
 import Appointment from "components/Appointment"
 import axios from 'axios';
-import { getAppointmentsForDay, getInterview } from "components/helpers/selectors";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "components/helpers/selectors";
 
 
 
 export default function Application(props) {
+
+  function bookInterview(id, interview) {
+    console.log(id, interview);
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview },
+
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    setState({
+      ...state,
+      appointments
+    });
+  }
   const [state, setState] = useState({
     day: "Monday",
     days: [],
@@ -28,13 +45,17 @@ export default function Application(props) {
       })
   }, [])
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const interviewersList = getInterviewersForDay(state, state.day);
   const setAppointments = dailyAppointments.map(appointment => {
     const interview = getInterview(state, appointment.interview);
+
     return (
       <Appointment
         key={appointment.id}
         {...appointment}
         interview={interview}
+        interviewers={interviewersList}
+        bookInterview={bookInterview}
       />)
   })
 
